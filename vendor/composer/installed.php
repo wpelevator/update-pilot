ca2f03ab4b5cb76866db19e5312fb7f119f5a6bf
Kaspars Dambis <hi@kaspars.net>
2024-10-28 19:59:03 +0200

    Trigger Satis update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpelevator/update-pilot',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1e132adb1fe647a277c16e23a2961f62aab8c601',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wpelevator/update-pilot' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1e132adb1fe647a277c16e23a2961f62aab8c601',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
