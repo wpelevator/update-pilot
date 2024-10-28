dcee8fbb92ec2c7989b997f36d877fa7371b45d7
Kaspars Dambis <hi@kaspars.net>
2024-10-28 23:16:31 +0200

    feat: Update Server can pull from Composer Repositories (#10)

---
+++ vendor/composer/installed.php
@@ -0,0 +1,27 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpelevator/update-pilot',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bcb9c44cf88602b1df7d94f6f4463086386b2c7b',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(
+            0 => '0.1.x-dev',
+        ),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wpelevator/update-pilot' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bcb9c44cf88602b1df7d94f6f4463086386b2c7b',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(
+                0 => '0.1.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
