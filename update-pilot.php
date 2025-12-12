a77c63d35b82e87ef36423240d07f1558bd3d5e8
Kaspars Dambis <hi@kaspars.net>
2025-12-12 21:08:46 +0200

    feat(plugins/update-pilot-server): Add FAIR package support (#125)

--- update-pilot.php
+++ update-pilot.php
@@ -4,9 +4,10 @@
  * Description: Get updates from third-party plugin and theme vendors.
  * Author: WP Elevator
  * Author URI: https://wpelevator.com
- * Version: 0.5.0
+ * Version: 0.5.1
  * Plugin URI: https://wpelevator.com/plugins/update-pilot
  * Update URI: https://updates.wpelevator.com/wp-json/update-pilot/v1/plugins
+ * Plugin ID: did:plc:etmyqc6vq2ruw7eltvpapvjk
  * Requires at least: 5.9
  * Requires PHP: 7.4
  * Network: true
