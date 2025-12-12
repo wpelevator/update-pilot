ba7332b1d8831e581c232e9c509e3d837953e04d
Kaspars Dambis <hi@kaspars.net>
2025-12-13 00:31:36 +0200

    fix(plugins/update-pilot): Add license to the plugin header for FAIR compatability (#128)

--- update-pilot.php
+++ update-pilot.php
@@ -4,13 +4,15 @@
  * Description: Get updates from third-party plugin and theme vendors.
  * Author: WP Elevator
  * Author URI: https://wpelevator.com
- * Version: 0.5.1
+ * Version: 0.5.2
  * Plugin URI: https://wpelevator.com/plugins/update-pilot
  * Update URI: https://updates.wpelevator.com/wp-json/update-pilot/v1/plugins
  * Plugin ID: did:plc:etmyqc6vq2ruw7eltvpapvjk
  * Requires at least: 5.9
  * Requires PHP: 7.4
  * Network: true
+ * License: GPL-2.0-or-later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 use WPElevator\Update_Pilot\Plugin;
