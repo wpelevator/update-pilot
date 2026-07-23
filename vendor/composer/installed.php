65c222c78d07f69f5f633842bdf1616189a8fed9
kasparsd <kasparsd@users.noreply.github.com>
2026-07-23 13:35:31 +0000

    Build and release

---
+++ vendor/composer/installed.php
@@ -0,0 +1,36 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpelevator/update-pilot',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2e6bf7aa97b409ecebc0fddbb9636f3075b7146c',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(
+            0 => '0.1.x-dev',
+        ),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v2.3.0',
+            'version' => '2.3.0.0',
+            'reference' => '12fb2dfe5e16183de69e784a7b84046c43d97e8e',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wpelevator/update-pilot' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2e6bf7aa97b409ecebc0fddbb9636f3075b7146c',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(
+                0 => '0.1.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
