6d591e0adf406b4c0b988c00ac3b664449e0f50b
kasparsd <kasparsd@users.noreply.github.com>
2026-08-21 12:20:54 +0000

    Build and release

---
+++ vendor-isolated/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0eea9c6223e98f18242ffa3d05112e06159cf40c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0eea9c6223e98f18242ffa3d05112e06159cf40c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wpelevator/update-client' => array(
+            'pretty_version' => '0.3.0',
+            'version' => '0.3.0.0',
+            'reference' => '35d3e927d8c49d90b91cd62173e4e33cd4b58935',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wpelevator/update-client',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
